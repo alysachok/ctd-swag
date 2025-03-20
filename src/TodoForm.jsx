71048849c7fccd3b04b35bce173fdbee12731607
Aleksandra Lysachok <aleksandra@lysachok.dev>
2025-03-20 10:14:29 -0400

    TodoForm Component was added.

---
+++ src/TodoForm.jsx
@@ -0,0 +1,11 @@
+function TodoForm() {
+  return (
+    <>
+      <label htmlFor="todoTitle">Todo</label>
+      <input type="text" id="todoTitle"></input>
+      <button>Add Todo</button>
+    </>
+  );
+}
+
+export default TodoForm;
