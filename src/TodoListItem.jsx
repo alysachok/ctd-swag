c435677e68b26d1b8f83ef79212bfabb1114d39c
Aleksandra Lysachok <aleksandra@lysachok.dev>
2025-03-25 12:50:43 -0400

    week 3 assigment done.

---
+++ src/TodoListItem.jsx
@@ -0,0 +1,13 @@
+const TodoListItem = ({ todos }) => {
+  return (
+    <>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>{todo.title}</li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default TodoListItem;
