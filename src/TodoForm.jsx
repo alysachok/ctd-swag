4ea4002ce3652fed6fd21357d1ccbadc60292951
Aleksandra Lysachok <aleksandra@lysachok.dev>
2025-03-25 12:03:29 -0400

    Type was added to the button.

--- src/TodoForm.jsx
+++ src/TodoForm.jsx
@@ -3,7 +3,7 @@ function TodoForm() {
     <>
       <label htmlFor="todoTitle">Todo</label>
       <input type="text" id="todoTitle"></input>
-      <button>Add Todo</button>
+      <button type="submit">Add Todo</button>
     </>
   );
 }
